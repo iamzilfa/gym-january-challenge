b4072cade3c679230643390e6a338d02effb67b5
iamzilfa <iamzilfa@gmail.com>
2023-02-25 16:45:58 +0200

    changes made

--- components/Layout/Footer.jsx
+++ components/Layout/Footer.jsx
@@ -1,6 +1,8 @@
 import Image from "next/image";
 import { FaFacebookF, FaInstagram } from "react-icons/fa";
 import Logo from "../../public/assets/vectors/logo-inverted-en.svg";
+import Usus from "../../public/assets/vectors/ususs.svg";
+import Usus2 from "../../public/assets/vectors/usus.svg";
 
 // import the library
 import { library } from "@fortawesome/fontawesome-svg-core";
@@ -23,14 +25,14 @@ const Footer = () => {
     <div className="bg-[#ddd]">
       <div className="container pt-[50px] pb-[20px] text-white bg-[#333] w-[1170px] mx-auto pl-[15px]">
         <div className="max-w-[960px] mx-auto">
-          <div className="flex gap-10 mb-[20px] -mx-[15px]">
+          <div className="flex sm:flex-col gap-10 mb-[20px] -mx-[15px]">
             <div>
               <Image src={Logo} alt="Logo" className="w-56" />
             </div>
-          
+
             <div className="grid grid-cols-7 gap-3">
               <div className="flex flex-col items-center space-y-1">
-                <div className="border rounded-full w-[60px] h-[60px] flex flex-col items-center justify-center">
+                <div className="border hover:bg-[#4267B2] hover:border-none rounded-full w-[60px] h-[60px] flex flex-col items-center justify-center">
                   <FontAwesomeIcon
                     icon={faFacebookF}
                     style={{ width: "15px" }}
@@ -39,8 +41,8 @@ const Footer = () => {
                 <p>Facebook</p>
               </div>
               <div className="flex flex-col items-center space-y-1">
-                <div className="border rounded-full w-[60px] h-[60px] flex flex-col items-center justify-center">
-                  <FontAwesomeIcon
+                <div className="border instaa hover:border-none  rounded-full w-[60px] h-[60px] flex flex-col items-center justify-center">
+                  <FontAwesomeIcon 
                     icon={faInstagram}
                     style={{ width: "20px" }}
                   />
@@ -48,13 +50,13 @@ const Footer = () => {
                 <p>Instagram</p>
               </div>
               <div className="flex flex-col items-center space-y-1">
-                <div className="border rounded-full w-[60px] h-[60px] flex flex-col items-center justify-center">
+                <div className="border hover:bg-[#1DA1F2] hover:border-none rounded-full w-[60px] h-[60px] flex flex-col items-center justify-center">
                   <FontAwesomeIcon icon={faTwitter} style={{ width: "20px" }} />
                 </div>
                 <p>Twitter</p>
               </div>
               <div className="flex flex-col items-center space-y-1">
-                <div className="border rounded-full w-[60px] h-[60px] flex flex-col items-center justify-center">
+                <div className="border hover:bg-[#5B4AE0] hover:border-none rounded-full w-[60px] h-[60px] flex flex-col items-center justify-center">
                   <FontAwesomeIcon
                     icon={faMastodon}
                     style={{ width: "20px" }}
@@ -63,13 +65,13 @@ const Footer = () => {
                 <p>Mastodon</p>
               </div>
               <div className="flex flex-col items-center space-y-1">
-                <div className="border rounded-full w-[60px] h-[60px] flex flex-col items-center justify-center">
+                <div className="border hover:border-none hover:bg-[#c4302b] rounded-full w-[60px] h-[60px] flex flex-col items-center justify-center">
                   <FontAwesomeIcon icon={faYoutube} style={{ width: "20px" }} />
                 </div>
                 <p>YouTube</p>
               </div>
               <div className="flex flex-col items-center space-y-1">
-                <div className="border rounded-full w-[60px] h-[60px] flex flex-col items-center justify-center">
+                <div className="border hover:bg-[#0072b1] hover:border-none rounded-full w-[60px] h-[60px] flex flex-col items-center justify-center">
                   <FontAwesomeIcon
                     icon={faLinkedin}
                     style={{ width: "20px" }}
@@ -78,8 +80,9 @@ const Footer = () => {
                 <p>LinkedIn</p>
               </div>
               <div className="flex flex-col items-center space-y-1">
-                <div className="border rounded-full w-[60px] h-[60px] flex flex-col items-center justify-center">
-                  <FontAwesomeIcon icon={faBlogger} style={{ width: "20px" }} />
+                <div className="border group hover:bg-white rounded-full w-[60px] h-[60px] flex flex-col items-center justify-center">
+                  <Image className="group-hover:hidden" src={Usus} alt="" />
+                  <Image className="hidden group-hover:block" src={Usus2} alt="" />
                 </div>
                 <p>USUS-blog</p>
               </div>
@@ -99,7 +102,7 @@ const Footer = () => {
                 ].map((e, i) => {
                   return (
                     <div key={i}>
-                      <p className="underline">{e}</p>
+                      <p className="underline hover:no-underline">{e}</p>
                     </div>
                   );
                 })}
@@ -118,7 +121,7 @@ const Footer = () => {
                 ].map((e, i) => {
                   return (
                     <div key={i}>
-                      <p className="underline">{e}</p>
+                      <p className="underline hover:no-underline">{e}</p>
                     </div>
                   );
                 })}
@@ -138,7 +141,7 @@ const Footer = () => {
                 ].map((e, i) => {
                   return (
                     <div key={i}>
-                      <p className="underline">{e}</p>
+                      <p className="underline hover:no-underline">{e}</p>
                     </div>
                   );
                 })}
@@ -156,7 +159,7 @@ const Footer = () => {
                 ].map((e, i) => {
                   return (
                     <div key={i}>
-                      <p className="underline">{e}</p>
+                      <p className="underline hover:no-underline">{e}</p>
                     </div>
                   );
                 })}
